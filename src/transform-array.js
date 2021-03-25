474737564ad4d1b23119aa35f21f367036d51834
Nikolai <vova1988open@gmail.com>
2021-03-26 00:25:23 +0300

    feat: implement 'transform-array'

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform( arr ) {
+let newArr = [];
+
+  
+  if(!Array.isArray(arr)){
+    throw new Error('Error');
+  }
+  if (arr.length === 0 || arr === 'Infinity') {
+    return [];
+  }
+    for(let i=0; i<arr.length; i++){
+      if(arr[i]==='--discard-next' && i !== arr.length-1 ){
+        i++;
+      }
+      else if(arr[i]==='--discard-prev' && arr[i - 2] !== '--discard-next'  && arr.length !== 0 ){
+        newArr.pop();
+      }else if(arr[i]==='--double-next' && i < arr.length - 1 ){
+        
+        newArr.push(arr[i+1]);
+      
+      }else if(arr[i]==='--double-prev' && arr[i-2] !== '--discard-next' && i !== 0 ){
+        newArr.push(arr[i-1]);
+      
+      }else if(arr[i] === '--discard-next' || arr[i] === '--discard-prev' || arr[i] === '--double-next' || arr[i] === '--double-prev'){ continue;
+      }else{
+        newArr.push(arr[i]);
+      }  
+  }
+  return newArr;
+
+
+  }
+
+
+
